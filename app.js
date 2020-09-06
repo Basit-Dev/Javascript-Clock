d22f8a89d8284cd514b986bcf7025ae9b03a6cba
pivanta <bas677@icloud.com>
2020-09-06 02:46:09 +0100

    1st commit

---
+++ app.js
@@ -0,0 +1,24 @@
+var myVar = setInterval(hours, 1000);
+
+function hours() {
+  var date = new Date();
+  var getHours = date.getHours();
+  var getMinutes = date.getMinutes();
+  var getSeconds = date.getSeconds();
+
+  document.getElementById("getDate").innerHTML =
+    "Today is " + date.toDateString();
+  document.getElementById("hours").innerHTML = getHours;
+  document.getElementById("minutes").innerHTML = getMinutes;
+  document.getElementById("seconds").innerHTML = getSeconds;
+
+  if (getHours < 10) {
+    document.getElementById("hours").innerHTML = "0" + getHours;
+  }
+  if (getMinutes < 10) {
+    document.getElementById("minutes").innerHTML = "0" + getMinutes;
+  }
+  if (getSeconds < 10) {
+    document.getElementById("seconds").innerHTML = "0" + getSeconds;
+  }
+}
